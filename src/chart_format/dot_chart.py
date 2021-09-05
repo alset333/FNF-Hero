962487b10b87e892a8d1737e3755666e90b394b1
Peter Maar <alset333@icloud.com>
2021-09-05 15:07:57 -0400

    License and Copyright Information

--- src/chart_format/dot_chart.py
+++ src/chart_format/dot_chart.py
@@ -1,3 +1,29 @@
+"""Handle .CHART files"""
+
+__copyright__ = 'Copyright 2021, Peter Maar'
+__author__ = 'Peter Maar <PeterMaar@protonmail.com>'
+__license__ = 'GNU Lesser General Public License Version 3'
+
+# __version__ = '0.0.0'
+# __date__ = 'D Month YYYY'
+# __credits__ = None
+
+#   This file is part of FNF Hero.
+#
+#   FNF Hero is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Lesser General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   FNF Hero is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Lesser General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public License
+#   along with FNF Hero.  If not, see <https://www.gnu.org/licenses/>.
+
+
 import note
 import chart
 
--- src/fnf_hero.py
+++ src/fnf_hero.py
@@ -1,16 +1,39 @@
 #!/usr/bin/env python3
 
-import os, sys
+"""FNF Hero. A utility to convert between .chart files for Clone Hero and .json files for Friday Night Funkin'.
+"""
 
-import re
+__copyright__ = 'Copyright 2021, Peter Maar'
+__author__ = 'Peter Maar <PeterMaar@protonmail.com>'
+__license__ = 'GNU Lesser General Public License Version 3'
+
+# __version__ = '0.0.0'
+# __date__ = 'D Month YYYY'
+# __credits__ = None
+
+#   This file is part of FNF Hero.
+#
+#   FNF Hero is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Lesser General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   FNF Hero is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Lesser General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public License
+#   along with FNF Hero.  If not, see <https://www.gnu.org/licenses/>.
+
+
+import os
+import sys
 
 import chart_format.dot_chart
 import chart_format.fnf_chart
 
-
-
 if __name__ == "__main__":
-
     DOT_CHART_FILE = os.path.normpath(sys.path[0] + "/../test_files/ttfaf.chart")
     FNF_CHART_FILE = os.path.normpath(sys.path[0] + "/../test_files/tutorial.json")
 
@@ -18,17 +41,12 @@ if __name__ == "__main__":
 
     dot_chart = chart_format.dot_chart.process_external_chart(open(DOT_CHART_FILE).read())
 
-
     print(fnf_chart)
     print(dot_chart)
-
 
     fnf_export = chart_format.fnf_chart.export_chart(dot_chart)
 
     print(fnf_export)
-
-
-
 
     None
 
